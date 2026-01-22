3f90012474c22fb08bb61293b7f52e1b507494a1
Huong Nguyen <huonguyenlt@gmail.com>
2026-01-23 00:12:22 +0700

    Create eks cluster

--- infra-aws-go/infra/main.go
+++ infra-aws-go/infra/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/ec2"
 	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/eks"
 	"github.com/pulumi/pulumi-aws/sdk/v7/go/aws/iam"
@@ -9,46 +11,89 @@ import (
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		// Create service role for EKS
-		eksRole, err := iam.NewRole(ctx, "eksClusterRole", &iam.RoleArgs{
-			AssumeRolePolicy: pulumi.String(`{
-				"Version": "2008-10-17",
-				"Statement": [{
-					"Sid": "",
-					"Effect": "Allow",
-					"Principal": {
-						"Service": "eks.amazonaws.com"
+		tmpJSON0, err := json.Marshal(map[string]any{
+			"Version": "2012-10-17",
+			"Statement": []map[string]any{
+				{
+					"Action": []string{
+						"sts:AssumeRole",
+						"sts:TagSession",
 					},
-					"Action": "sts:AssumeRole"
-				}]
-			}`),
-			ManagedPolicyArns: pulumi.StringArray{
-				pulumi.String("arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"),
+					"Effect": "Allow",
+					"Principal": map[string]any{
+						"Service": "eks.amazonaws.com",
+					},
+				},
 			},
 		})
 		if err != nil {
 			return err
 		}
 
-		// Get all subnets in the VPC, filter out those in us-east-1e, and pick 2 random subnet IDs
+		json0 := string(tmpJSON0)
+		cluster, err := iam.NewRole(ctx, "clusterRole", &iam.RoleArgs{
+			Name:             pulumi.String("eksClusterRole"),
+			AssumeRolePolicy: pulumi.String(json0),
+		})
+		if err != nil {
+			return err
+		}
+
+		clusterAmazonEKSClusterPolicy, err := iam.NewRolePolicyAttachment(
+			ctx,
+			"cluster_AmazonEKSClusterPolicy",
+			&iam.RolePolicyAttachmentArgs{
+				PolicyArn: pulumi.String("arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"),
+				Role:      cluster.Name,
+			},
+		)
+		if err != nil {
+			return err
+		}
+
 		t := true
-		vpc, err := ec2.LookupVpc(ctx, &ec2.LookupVpcArgs{Default: &t})
+		vpc, err := ec2.LookupVpc(ctx, &ec2.LookupVpcArgs{
+			Default: &t,
+		})
 		if err != nil {
 			return err
 		}
-		subnets, err := ec2.GetSubnets(ctx, &ec2.GetSubnetsArgs{
-			Filters: []ec2.GetSubnetsFilter{
-				{Name: "vpc-id", Values: []string{vpc.Id}},
-				{Name: "availability-zone", Values: []string{"us-east-1e"}},
-			},
-		})
 
-		// Create EKS cluster
-		cluster, err := eks.NewCluster(ctx, "demo-eks", &eks.ClusterArgs{
-			RoleArn: pulumi.StringInput(eksRole.Arn),
-			AccessConfig: &eks.ClusterAccessConfigArgs{
-				AuthenticationMode: pulumi.String("API_AND_CONFIG_MAP"),
+		subnetIds, err := ec2.GetSubnets(ctx, &ec2.GetSubnetsArgs{
+			Filters: []ec2.GetSubnetsFilter{
+				{
+					Name:   "vpc-id",
+					Values: []string{vpc.Id},
+				},
+				{
+					Name: "availability-zone",
+					Values: []string{
+						"us-east-1a",
+						"us-east-1b",
+					},
+				},
 			},
 		})
+		if err != nil {
+			return err
+		}
+
+		_, err = eks.NewCluster(ctx, "demoEks", &eks.ClusterArgs{
+			Name: pulumi.String("demo-eks"),
+			AccessConfig: &eks.ClusterAccessConfigArgs{
+				AuthenticationMode: pulumi.String("API"),
+			},
+			RoleArn: cluster.Arn,
+			Version: pulumi.String("1.33"),
+			VpcConfig: &eks.ClusterVpcConfigArgs{
+				SubnetIds: pulumi.ToStringArray(subnetIds.Ids),
+			},
+		}, pulumi.DependsOn([]pulumi.Resource{
+			clusterAmazonEKSClusterPolicy,
+		}))
+		if err != nil {
+			return err
+		}
+		return nil
 	})
 }
