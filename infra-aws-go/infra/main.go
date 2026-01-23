cb8748dc590e6dab035a0948e1fddb5cd738a17f
Huong Nguyen <huonguyenlt@gmail.com>
2026-01-24 00:05:46 +0700

    add worker node instance profile and role

--- infra-aws-go/infra/main.go
+++ infra-aws-go/infra/main.go
@@ -31,7 +31,7 @@ func main() {
 		}
 
 		json0 := string(tmpJSON0)
-		cluster, err := iam.NewRole(ctx, "clusterRole", &iam.RoleArgs{
+		cluster, err := iam.NewRole(ctx, "cluster-role", &iam.RoleArgs{
 			Name:             pulumi.String("eksClusterRole"),
 			AssumeRolePolicy: pulumi.String(json0),
 		})
@@ -41,7 +41,7 @@ func main() {
 
 		clusterAmazonEKSClusterPolicy, err := iam.NewRolePolicyAttachment(
 			ctx,
-			"cluster_AmazonEKSClusterPolicy",
+			"cluster-AmazonEKSClusterPolicy",
 			&iam.RolePolicyAttachmentArgs{
 				PolicyArn: pulumi.String("arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"),
 				Role:      cluster.Name,
@@ -78,7 +78,7 @@ func main() {
 			return err
 		}
 
-		_, err = eks.NewCluster(ctx, "demoEks", &eks.ClusterArgs{
+		eksCluster, err := eks.NewCluster(ctx, "demo-eks", &eks.ClusterArgs{
 			Name: pulumi.String("demo-eks"),
 			AccessConfig: &eks.ClusterAccessConfigArgs{
 				AuthenticationMode: pulumi.String("API"),
@@ -94,6 +94,91 @@ func main() {
 		if err != nil {
 			return err
 		}
+
+		tmpJSON1, err := json.Marshal(map[string]any{
+			"Version": "2012-10-17",
+			"Statement": []map[string]any{
+				{
+					"Action": []string{
+						"sts:AssumeRole",
+						"sts:TagSession",
+					},
+					"Effect": "Allow",
+					"Principal": map[string]any{
+						"Service": "ec2.amazonaws.com",
+					},
+				},
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		json1 := string(tmpJSON1)
+		nodeRole, err := iam.NewRole(ctx, "node-role", &iam.RoleArgs{
+			Name:             pulumi.String("nodeRole"),
+			AssumeRolePolicy: pulumi.String(json1),
+			ManagedPolicyArns: pulumi.StringArray{
+				pulumi.String("arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"),
+				pulumi.String("arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"),
+				pulumi.String("arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"),
+				pulumi.String("arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
+		// nodeInstanceProfile, err := iam.NewInstanceProfile(
+		_, err = iam.NewInstanceProfile(
+			ctx,
+			"node-instance-profile",
+			&iam.InstanceProfileArgs{
+				Role: nodeRole.Name,
+			},
+		)
+
+		// nodeSecurityGroup, err := ec2.NewSecurityGroup(
+		_, err = ec2.NewSecurityGroup(
+			ctx,
+			"node-security-group",
+			&ec2.SecurityGroupArgs{
+				Description: pulumi.String("Security group for all nodes in the cluster"),
+				VpcId:       pulumi.StringPtr(vpc.Id),
+				Tags: pulumi.StringMap{
+					"kubernetes.io/cluster/demo-eks": pulumi.String("owned"),
+				},
+				Ingress: ec2.SecurityGroupIngressArray{
+					ec2.SecurityGroupIngressArgs{
+						Description: pulumi.StringPtr("Allow node to communicate with each other"),
+						FromPort:    pulumi.Int(0),
+						ToPort:      pulumi.Int(0),
+						Protocol:    pulumi.String("-1"),
+						Self:        pulumi.BoolPtr(true),
+					},
+					ec2.SecurityGroupIngressArgs{
+						Description: pulumi.StringPtr(
+							"Allow worker Kubelets and pods to receive communication from the cluster control plane",
+						),
+						FromPort:       pulumi.Int(1025),
+						Protocol:       pulumi.String("tcp"),
+						ToPort:         pulumi.Int(65535),
+						SecurityGroups: eksCluster.VpcConfig.SecurityGroupIds(),
+					},
+					ec2.SecurityGroupIngressArgs{
+						Description: pulumi.StringPtr(
+							"Allow pods running extension API servers on port 443 to receive communication from cluster control plane",
+						),
+						FromPort:       pulumi.Int(443),
+						ToPort:         pulumi.Int(443),
+						SecurityGroups: eksCluster.VpcConfig.SecurityGroupIds(),
+						Protocol:       pulumi.String("tcp"),
+					},
+				},
+			},
+		)
+
+		// _, err = ec2.NewSecurityGroupRule(ctx, "cluster-sg-rule-1", &ec2.SecurityGroupRuleArgs{})
 		return nil
 	})
 }
